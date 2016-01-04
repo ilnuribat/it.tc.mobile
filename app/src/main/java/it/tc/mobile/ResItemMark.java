d206ffa72bd09a4991b5f2174a620c24b1acb7c5
ilnuribat <ilnuribat@gmail.com>
2016-01-04 20:14:03 +0500

    this class will declare the item from list - resource

---
+++ app/src/main/java/it/tc/mobile/ResItemMark.java
@@ -0,0 +1,38 @@
+package it.tc.mobile;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by itibatullin on 29.12.2015.
+ */
+public class ResItemMark {
+    private String nameLastname;
+    private int ID;
+    private int markID;
+    private int intentMark;
+
+    ResItemMark (JSONObject jsonObject) {
+        try {
+            nameLastname = jsonObject.getString("nameLastname");
+            ID = jsonObject.getInt("id");
+            markID = jsonObject.getInt("mark");
+        } catch (JSONException ignored) {}
+    }
+    public String getName() {
+        return nameLastname;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public int getMarkID() {
+        return markID;
+    }
+
+    public void setMarkID(int newMark) {
+        //Может тут делать запрос на добавление новой оценки
+        intentMark = newMark;
+    }
+}

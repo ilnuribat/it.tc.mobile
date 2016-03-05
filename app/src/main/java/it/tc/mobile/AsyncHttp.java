1d1eaf68d247f2e4dc76e0e1b484b88d9ecd21ae
ilnuribat <ilnuribat@gmail.com>
2016-03-06 04:56:06 +0500

    server IP was changed

--- app/src/main/java/it/tc/mobile/AsyncHttp.java
+++ app/src/main/java/it/tc/mobile/AsyncHttp.java
@@ -22,7 +22,7 @@ class AsyncHttp extends AsyncTask<String, Void, String> {
         String method = urls[0];
         String url = urls[1];
         String API_KEY = "&apikey=6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b";
-        String IP = "http://52.27.138.37:8080";
+        String IP = "http://52.34.168.159:8080";
         if (method.equals("GET"))
             return downloadUrlGET(IP + url + API_KEY);
         //POST METHOD
